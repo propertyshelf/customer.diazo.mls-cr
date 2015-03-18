e28d3dee0dcd61830e8f1df193d7ea43f5483e9e
Kantanart <kantanat@propertyshelf.com>
2015-03-18 15:47:08 +0700

    Header , fonter responsive
    - Style header support responsive
    - Style footer support responsive

--- src/customer/diazo/mlscr_com/diazo_resources/static/main.js
+++ src/customer/diazo/mlscr_com/diazo_resources/static/main.js
@@ -1,5 +1,22 @@
-jQuery(function(jq) {
+function Descript_and_title(){
+	$(".header_title, .header_descript, .site-social").insertAfter(".logo-wrapper .logo");
+}
 
-  // Add your theme JS code here.
+function Add_class_breadcrumbs(){
+	$("#wrapper-inner #portal-breadcrumbs").addClass("container");
+}
 
-});
+function doormatColumn_double(){
+	$(".doormatColumn .doormatSection .doormatSectionBody .doormatColumn").removeClass();
+}
+
+$(document).ready(function(){
+    if($('.header_title').length>0){
+        
+    }
+
+    if($('.doormatColumn .doormatSection .doormatSectionBody .doormatColumn').length>0){
+        doormatColumn_double();
+    }
+    Add_class_breadcrumbs();
+})
