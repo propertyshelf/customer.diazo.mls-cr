55767aa9965f1b63223c1e8e138ca5b80c2da117
Kantanart <kantanat@propertyshelf.com>
2015-03-19 20:22:47 +0700

    Fixed about double class doormat
    Done.

--- src/customer/diazo/mlscr_com/diazo_resources/static/main.js
+++ src/customer/diazo/mlscr_com/diazo_resources/static/main.js
@@ -6,17 +6,9 @@ function Add_class_breadcrumbs(){
 	$("#wrapper-inner #portal-breadcrumbs").addClass("container");
 }
 
-function doormatColumn_double(){
-	$(".doormatColumn .doormatSection .doormatSectionBody .doormatColumn").removeClass();
-}
-
 $(document).ready(function(){
     if($('.header_title').length>0){
         
-    }
-
-    if($('.doormatColumn .doormatSection .doormatSectionBody .doormatColumn').length>0){
-        doormatColumn_double();
     }
     Add_class_breadcrumbs();
 })
