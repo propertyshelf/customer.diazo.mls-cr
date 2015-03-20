29e93ce3a3b15c51c8a89a70d06970407cc3b95f
Kantanart <kantanat@propertyshelf.com>
2015-03-20 16:51:58 +0700

    Make div full width background
    - Add class on Cover setting
    - full_mls_search
    - embed_im
    - Add Jquery for move this class to ps_full_width
    - Style Background full width and listing search on full screen

--- src/customer/diazo/mlscr_com/diazo_resources/static/main.js
+++ src/customer/diazo/mlscr_com/diazo_resources/static/main.js
@@ -1,14 +1,31 @@
 function Descript_and_title(){
-	$(".header_title, .header_descript, .site-social").insertAfter(".logo-wrapper .logo");
+    $(".header_title, .header_descript, .site-social").insertAfter(".logo-wrapper .logo");
+}
+
+function div_full_width(){
+    if($('.full_mls_search').length>0){   
+        $('#wrapper-inner').after('<div class="ps_full_width">&nbsp;</div>');
+        $('.full_mls_search').prependTo('.ps_full_width');
+    }
+}
+function embed_im_map(){
+    embedmap = $(".embed_im").children("div.full_map");
+    
+    if($('.full_mls_search').length>0){   
+        $(embedmap).prependTo(".ps_full_width .full_mls_search");
+    }
 }
 
 function Add_class_breadcrumbs(){
+    console.log("XX");
 	$("#wrapper-inner #portal-breadcrumbs").addClass("container");
 }
 
 $(document).ready(function(){
-    if($('.header_title').length>0){
-        
+    if($('body.template-view').length>0){   
+        div_full_width();
+        embed_im_map();
     }
+
     Add_class_breadcrumbs();
 })
